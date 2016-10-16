8c21ea748c54a0f40a41947f30ed3ee8e984fe12
kenta-s <knt01222@gmail.com>
2016-10-16 10:08:24 +0900

    Separate some functions to make it more readable

--- MySQLConnection.java
+++ MySQLConnection.java
@@ -4,14 +4,11 @@ public class MySQLConnection {
     public ResultSet[] main(String args[]) {
         Connection con = null;
         ResultSet[] resultSet = new ResultSet[args.length];
-        String host = System.getenv("DB_HOST");
-        String port = System.getenv("DB_PORT");
-        String database = System.getenv("DB_NAME");
         String user = System.getenv("DB_USER");
         String password = System.getenv("DB_PASSWORD");
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, user, password);
+            con = DriverManager.getConnection(getConnectionString(), user, password);
             Statement statement = con.createStatement();
             for (int i = 0; i < args.length; i++){
                String sql = "select * from " + args[i];
@@ -32,5 +29,11 @@ public class MySQLConnection {
             }
         }
       return resultSet;
+    }
+    private static String getConnectionString(){
+        String host = System.getenv("DB_HOST");
+        String port = System.getenv("DB_PORT");
+        String database = System.getenv("DB_NAME");
+        return "jdbc:mysql://" + host + ":" + port + "/" + database;
     }
 }
